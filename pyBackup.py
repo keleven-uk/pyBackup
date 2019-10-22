fc1ffd1e48805fbc30b2a92c3176c40d53424fbb
keleven-uk <github@keleven.co.uk>
2019-10-22 21:34:41 +0100

    version = 1.0.3.
    Added the ability to remove empty directories.
    Added the abilty to print bytes in a human readable format i.e. in KB or
    MB etc.
    
    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- pyBackup.py
+++ pyBackup.py
@@ -4,28 +4,27 @@
 #                                                                                                             #
 #  The backup is a mirrored backup, that is after the process destDir will be a perfect mirror of sourceDir.  #
 #                                                                                                             #
-#  Usage: pyBackup [--help, --test] sourceDir destDir                                                         #
+#  Usage: pyBackup [--help --test --license --version] -s sourceDir -d destDir                                #
 #                                                                                                             #
 #  October 2019                Kevin Scott                                                                    #
 #                                                                                                             #
 #                                                                                                             #
-#   V1.00   17 Oct.2019     Will copy files that are not in the destination.                                  #
+#   V1.01   17 Oct.2019     Will copy files that are not in the destination.                                  #
+#   V1.02   20 Oct.2019     Added copy of file size and date change.  Added reverse scan.                     #
 #                                                                                                             #
 ###############################################################################################################
 #    Copyright (C) <2019>  <Kevin Scott>                                                                      #
 #                                                                                                             #
-#    This program is free software: you can redistribute it and/or modify it                                  #
-#    under the terms of the GNU General Public License as published by                                        #
-#    the Free Software Foundation, either version 3 of the License, or                                        #
-#    (at your option) any later version.                                                                      #
+#    This program is free software: you can redistribute it and/or modify it under the terms of the           #
+#    GNU General Public License as published by the Free Software Foundation, either version 3 of the         #`
+#    License, or (at your option) any later version.                                                          #
 #                                                                                                             #
-#    This program is distributed in the hope that it will be useful,                                          #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of                                           #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                            #
+#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without        #
+#    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
 #    GNU General Public License for more details.                                                             #
 #                                                                                                             #
-#    You should have received a copy of the GNU General Public License                                        #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.                                    #
+#    You should have received a copy of the GNU General Public License along with this program.               #
+#    If not, see <http://www.gnu.org/licenses/>.                                                              #
 #                                                                                                             #
 ###############################################################################################################
 
@@ -54,6 +53,7 @@ class Results():
         self.dateNumber   = 0
         self.deleteSize   = 0
         self.deleteNumber = 0
+        self.emptyDirs    = 0
 
 ############################################################################################## copyFiles ######
 
@@ -61,7 +61,7 @@ def copyFiles(sourceFileName, destFileName, mode, test):
     """  If file does not exist in the destination directory, then copy the file.
          If the destination path does not exit, the directory path is created.
 
-         will only copy is test is set to false.
+         Will only copy is test is set to false.
     """
     destDir = os.path.split(destFileName)[0]
     if not os.path.exists(destDir):
@@ -76,18 +76,18 @@ def copyFiles(sourceFileName, destFileName, mode, test):
         print("Copying {} :: {}".format(mode, sourceFileName))
         if not test: shutil.copy(sourceFileName, destFileName)
     except (IOError, os.error) as error:
-        sys.stderr.write(sourceFileName + " : could not copy {0}".format(error))
+        print("ERROR :: {0} : could not copy {1}".format(sourceFileName, error))
 
 #################################################################################### comparForwardFiles ######
 
 def compareForwardFiles(sourceFileName, destFileName, test):
     """  Called when forward checking the original source against the original destination.
     
-    Compares two files, one in the source directory and the second in the destination directory.
-    Then copies the files depending upon the result.  
-    Will only copy the file is it does not exist in the destination or has been altered,
-    or that the file size is different in source and destination,
-    or that the file date is later in source then destination.
+         Compares two files, one in the source directory and the second in the destination directory.
+         Then copies the files depending upon the result.  
+         Will only copy the file is it does not exist in the destination or has been altered,
+         or that the file size is different in source and destination,
+         or that the file date is later in source then destination.
     """
     if not os.path.exists(destFileName): 
         mode = "file does not exist in destination"
@@ -109,13 +109,13 @@ def compareForwardFiles(sourceFileName, destFileName, test):
 
 ################################################################################## compareReverseFiles ######
 
-def compareReverseFiles(sourceFileName, destFileName, test):
+def compareReverseFiles(sourceFileName, destFileName, test, root):
     """  Called when reverse checking the original destination against the original source.
     
-    Compares two files, one in the source directory and the second in the destination directory.
-    Then deletes the files if in the source directory and not in the destination.  
+         Compares two files, one in the source directory and the second in the destination directory.
+         Then deletes the files if in the source directory and not in the destination.  
 
-    will only copy is test is set to false.
+         will only copy is test is set to false.
     """
     if not os.path.exists(destFileName): 
         mode = "file does not exist in Source"
@@ -126,7 +126,19 @@ def compareReverseFiles(sourceFileName, destFileName, test):
             print("Deleting {} :: {}".format(mode, sourceFileName))
             if not test: os.remove(sourceFileName)
         except (IOError, os.error) as error:
-            sys.stderr.write(destFileName + " : could not delete {0}".format(error))
+            print("ERROR :: {0} : could not delete {1}".format(destFileName, error))
+
+############################################################################################## removeDir ######
+def removeDir(path):
+    """  removes a empty directory.
+    """
+
+    print("removing empty directory {}".format(path))
+    try:
+        os.rmdir(path)
+        CopyResults.emptyDirs += 1
+    except (IOError, os.error) as error:
+        print("ERROR :: {0} : could not delete {1}".format(path, error))
 
 ################################################################################################# backup ######
 
@@ -140,12 +152,12 @@ def backup(sourceDir, destDir, direction, test):
             newRoot = root.replace(sourceDir, destDir)
             destFileName   = os.path.join(newRoot, name)
 
-            #print("{} :: {}".format(sourceFileName, destFileName))
-
             if direction == "forward":
                 compareForwardFiles(sourceFileName, destFileName, test)   #   original.source -> original.destination
             else:
-                compareReverseFiles(sourceFileName, destFileName, test)   #   original.destination -> original.source
+                compareReverseFiles(sourceFileName, destFileName, test, root)   #   original.destination -> original.source
+
+        if not os.listdir(root): removeDir(root)
 
 ########################################################################################### printResults ######
 
@@ -153,18 +165,32 @@ def printResults():
     """  Print out the results of the backup.
     """
     print()
-    print("Copied %s file[s] not in destination [Copied], {} bytes".format(CopyResults.copyNumber,   CopyResults.copySize))
-    print("Copied %s file[s] that size has changed      , {} bytes".format(CopyResults.sizeNumber,   CopyResults.sizeSize))
-    print("Copied %s file[s] that date has changed      , {} bytes".format(CopyResults.dateNumber,   CopyResults.dateSize))
-    print("Copied %s file[s] not in source [deleted]    , {} bytes".format(CopyResults.deleteNumber, CopyResults.deleteSize))
+    print("Copied {0} file[s] not in destination [Copied], {1}".format(CopyResults.copyNumber,   GetHumanReadable(CopyResults.copySize)))
+    print("Copied {0} file[s] that size has changed      , {1}".format(CopyResults.sizeNumber,   GetHumanReadable(CopyResults.sizeSize)))
+    print("Copied {0} file[s] that date has changed      , {1}".format(CopyResults.dateNumber,   GetHumanReadable(CopyResults.dateSize)))
+    print("Copied {0} file[s] not in source [deleted]    , {1}".format(CopyResults.deleteNumber, GetHumanReadable(CopyResults.deleteSize)))
+    print("Empty directories deleted                    , {0}".format(CopyResults.emptyDirs))
+
+####################################################################################### GetHumanReadable ######
+def GetHumanReadable(size,precision=2):
+    """  Returns the number of bytes in a more readable form.
+         Nicked from the internet.
+    """
+    suffixes=['B','KB','MB','GB','TB']
+    suffixIndex = 0
+    while size > 1024:
+        suffixIndex += 1        #  increment the index of the suffix
+        size = size/1024.0      #  apply the division
+    return "%.*f %s"%(precision,size,suffixes[suffixIndex])
 
 ########################################################################################### printSortLicense ######
 def printShortLicense():
     print("""
-    PyBackup  Copyright (C) 2019  Kevin Scott
-    This program comes with ABSOLUTELY NO WARRANTY; for details type `pyBackup -l'.
-    This is free software, and you are welcome to redistribute it under certain conditions.
-    """, end="")
+PyBackup {}   Copyright (C) 2019  Kevin Scott
+This program comes with ABSOLUTELY NO WARRANTY; for details type `pyBackup -l'.
+This is free software, and you are welcome to redistribute it under certain conditions.
+    """.format(__version__))
+
 ########################################################################################### printLongLicense ######
 def printLongLicense():
     print("""
@@ -183,13 +209,11 @@ def printLongLicense():
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     """, end="")
-############################################################################################### __main__ ######
 
-CopyResults = Results()     #   Creates the results class.
-
-if __name__ == "__main__":
-    start_time = time.time()
-
+############################################################################################## parseArgs ######
+def parseArgs():
+    """  Process the command line arguments.
+    """
     parser = argparse.ArgumentParser(
         formatter_class = argparse.RawTextHelpFormatter,
         description=textwrap.dedent("""\
@@ -209,36 +233,37 @@ if __name__ == "__main__":
 
     args   = parser.parse_args()
 
-    sourceDir = args.sourceDir
-    destDir   = args.destDir
-    test      = args.test
-    license   = args.license
-
-    if license:
+    if args.license:
         printLongLicense()
         exit(0)
 
-    print(__version__)
-    print("PyBackup {} - A backup script.".format(__version__))
-    print("---------------------------", end="")
     printShortLicense()
-    print("---------------------------")
 
-    if (sourceDir == "") or (destDir == ""):
+    if (args.sourceDir == "") or (args.destDir == ""):
         print()
         print("Run build.py --help for usage")
         exit(2)
 
+    return (args.sourceDir, args.destDir, args.test)
+
+############################################################################################### __main__ ######
+
+CopyResults = Results()     #   Creates the results class.
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    sourceDir, destDir, test = parseArgs()
 
     if os.path.isdir(sourceDir):
         if test:
-            print("Backing up {} to {} n test mode".format(sourceDir, destDir))
+            print("Backing up {0} -> {1} n test mode".format(sourceDir, destDir))
         else:
-            print("Backing up {} to {} ".format(sourceDir, destDir))
+            print("Backing up {0} -> {1} ".format(sourceDir, destDir))
 
         backup(sourceDir, destDir, "forward", test)
         print("-----------------------------------------------------------------")
-        print("Reverse Scan")
+        print("Reverse Scan :: {1} -> {0}".format(sourceDir, destDir))
         print("-----------------------------------------------------------------")
         backup(destDir, sourceDir, "reverse", test)
         printResults()
@@ -249,5 +274,5 @@ if __name__ == "__main__":
 
     print()
     elapsed_time_secs = time.time() - start_time
-    print("Completed %s" % datetime.timedelta(seconds = elapsed_time_secs))
+    print("Completed {}".format(datetime.timedelta(seconds = elapsed_time_secs)))
     print()
